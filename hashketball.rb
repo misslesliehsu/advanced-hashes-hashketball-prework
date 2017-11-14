a6b10b934320730a2631623c4a816198570f8739
Leslie Hsu <miss.leslie.hsu@gmail.com>
2017-11-14 15:18:25 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -162,7 +162,9 @@ end
 def player_numbers(team)
   numbers = []
   game_hash.each do |side, infos|
+    binding.pry
     #below, why do we have to have 'game_hash'?  aren't we already inside :home?
+    #if side[:team_name]
     if game_hash[side][:team_name] == team
       game_hash[side][:players].each do |player_name, stat_keys|
         numbers << stat_keys[:number]
